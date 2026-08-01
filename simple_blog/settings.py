1e5a42af8bd25d548d0d3ae1479afe61006e04fa
swethamanju2002 <swethaharish2222@gmail.com>
2026-08-01 15:55:31 +0530

    Added Render host to ALLOWED_HOSTS

--- simple_blog/settings.py
+++ simple_blog/settings.py
@@ -23,10 +23,13 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-wkj+(&7*-o6*nrpyjx4_ojhb-^5t79j070l%3sr(c&#oob686$'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
-
+ALLOWED_HOSTS = [
+    "127.0.0.1",
+    "localhost",
+    "simple-blog-rdqd.onrender.com",
+]
 
 # Application definition
 
