ebad7e6934585142df38db17feb3b41d8ead7048
swethamanju2002 <swethaharish2222@gmail.com>
2026-08-03 10:51:54 +0530

    Fix WhiteNoise middleware

--- simple_blog/settings.py
+++ simple_blog/settings.py
@@ -47,6 +47,7 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware',
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
@@ -120,7 +121,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/6.0/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
 STATICFILES_DIRS = [BASE_DIR / 'static']  # Our own CSS/JS during development
 STATIC_ROOT = BASE_DIR / "staticfiles"
 
